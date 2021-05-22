dc36fdaf35c2620dcab18ef9a3c733708d39c558
ccgrace <77289566+ccgrace@users.noreply.github.com>
2021-05-21 21:23:45 -0400

    trying to get map to show up

--- Leaflet-Step-1/js/logic.js
+++ Leaflet-Step-1/js/logic.js
@@ -1,16 +1,14 @@
 var myMap = L.map("mapid", {
     center: [38.2700, -100.8603],
-    zoom: 13
+    zoom: 5
   });
 
 // Adding tile layer
-L.tileLayer("https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
-  attribution: "© <a href='https://www.mapbox.com/about/maps/'>Mapbox</a> © <a href='http://www.openstreetmap.org/copyright'>OpenStreetMap</a> <strong><a href='https://www.mapbox.com/map-feedback/' target='_blank'>Improve this map</a></strong>",
-  tileSize: 512,
-  maxZoom: 18,
-  zoomOffset: -1,
-  id: "mapbox/streets-v11",
-  accessToken: API_KEY
+L.tileLayer("https://api.mapbox.com/styles/v1/mapbox/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
+    attribution: "Map data &copy; <a href=\"https://www.openstreetmap.org/\">OpenStreetMap</a> contributors, <a href=\"https://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA</a>, Imagery © <a href=\"https://www.mapbox.com/\">Mapbox</a>",
+    maxZoom: 18,
+    id: "light-v10",
+    accessToken: API_KEY
 }).addTo(myMap);
 
 
